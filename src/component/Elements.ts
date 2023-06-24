ce6ec61d76351007d51ae5c0cebb383ded3a9faa
karamelev <karamelev@tut.by>
2023-06-24 18:22:01 +0300

    locators

--- src/component/Elements.ts
+++ src/component/Elements.ts
@@ -4,10 +4,10 @@ export class Elements {
     education = $("div .cc-nav [data-type='nav_id96']");
     scalpTrading = $(".grid [data-type=nav_id530]");
     loginTextInFieldLogin = $("#l_overlay .h1");
-    LinkSingUpnFieldLogin = $("//a[contains(text(),'Sign up')]");
+    LinkSingUpnFieldLogin = $("#l_overlay .l_btn_signup");
     fieldEmailAddress = $('#l_f_email input[type="email"]');
     fieldPassword = $('#l_f_pass input[type="password"]');
-    linkForgotPassword = $('//a[contains(text(),'Forgot password?')]');
+    linkForgotPassword = $('#l_overlay a.l_btn_forgot');
     buttonContinue = $('#l_overlay form [type = submit]');
-   checkBox = $('#l_overlay input[type = checkbox]');
+    checkBox = $('#l_overlay input[type = checkbox]');
 };
