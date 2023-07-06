7cd309840f1d5ce1eeccc26a4a3ec54c699e47e4
karamelev <karamelev@tut.by>
2023-07-06 17:35:18 +0300

    TC verification button Login in Headers

---
+++ src/component/LoginForm.ts
@@ -0,0 +1,13 @@
+class LoginForm {
+    loginTextInField = ".overlay:not(.hidden) .modal .form-container-small-header >div";
+    linkSingUp = ".overlay:not(.hidden) .modal .l_btn_signup";
+    fieldEmailAddress = '.overlay:not(.hidden) .modal input[type="email"]';
+    fieldPassword = '.overlay:not(.hidden) .modal input[type="password"]';
+    linkForgotPassword = '.overlay:not(.hidden) .modal a.l_btn_forgot';
+    buttonContinue = '.overlay:not(.hidden) .modal form [type = submit]';
+    checkBox = '.overlay:not(.hidden) .modal  input[type = checkbox]';
+    inputWithAccauntGoogle = '.overlay:not(.hidden) .modal .socialsLogin__btn--google';
+    inputWithAccauntApple = '.overlay:not(.hidden) .modal .sn-login-ap';
+    inputWithAccauntFacebook = '.overlay:not(.hidden) .modal .sn-login-fb';
+}
+export const loginForm = new LoginForm();
--- test/TestEducationScalpTrading_Smoke.ts
+++ test/TestEducationScalpTrading_Smoke.ts
@@ -1,30 +1,60 @@
 import {capitalCom} from "../src/component/CapitalComApp";
-
+import {SignUp, baseUrl, login} from "../src/data/TestData";
+import {loginForm } from "../src/component/LoginForm";
+import {expect} from "chai";
 
 describe ('TC_11.03.06_01 | Education > Menu Item [Scalp Trading]', async function () {
     before(async function() {
-        await capitalCom.open;
+        await browser.maximizeWindow();
+        await browser.url(baseUrl);     
     });
 
-    it ('test', async function () {
-        await capitalCom.loginButtonInHeader.clickButtonLogin();
-        // $("").setValue("") заполнить поле значением
+    it ('test verification Name of the form “Login“', async function () {
+        await capitalCom.clickLoginHeader();
+        const textLoginInPopupLogin = await $(loginForm.loginTextInField).getText();
+        expect (textLoginInPopupLogin).to.equal(login);
     });
-    // it ('Compute Engine', async function (){
-    //     await google.searchResults.clickLinkWithText();
-    //     await google.computeEngine.clickButtonComputeEngine();
-    //     // await google.calculator.selectMenuOption('');
-    // });
-    // it ('fill in the field Number of Instances', async function (){
+    it ('test verification link “Sign up” in form "Login"', async function () {
+       const linkSingUpInLoginPopup = await $(loginForm.linkSingUp).getTagName();
+       const textlinkSingUpInLoginPopup = await $(loginForm.linkSingUp).getText();
+       expect (linkSingUpInLoginPopup).to.equal('a');
+       expect (textlinkSingUpInLoginPopup).to.equal(SignUp);
+    });
+    it ('test verification Input field “Email address“” in form "Login"', async function () {
+        const fieldEmailAddressInLoginPopup = await $(loginForm.fieldEmailAddress).isDisplayed();
+        expect(fieldEmailAddressInLoginPopup).to.equal(true);
+     });
+     it ('test verification Input field “Password“ in form "Login"', async function () {
+        const fieldPasswordInLoginPopup = await $(loginForm.fieldPassword).isDisplayed();
+        expect(fieldPasswordInLoginPopup).to.equal(true);
+     });
+     it ('test verification Link “Forgot password?“ in form "Login"', async function () {
+        const linkForgotPasswordInLoginPopup = await $(loginForm.linkForgotPassword).getTagName();
+        const textlinkForgotPassword = await (await $(loginForm.linkForgotPassword)).getText();
+        expect(linkForgotPasswordInLoginPopup).to.equal('a');
+        expect(textlinkForgotPassword).to.equal('Forgot password?');
+     });
+     it ('test verification The button “Continue“ in form "Login"', async function () {
+        const buttonContinueInLoginPopup = await $(loginForm.buttonContinue).isDisplayed();
+        expect(buttonContinueInLoginPopup).to.equal(true);
+     });
+     it ('test verification Check box “Log me out after 7 days“ in form "Login"', async function () {
+        const checkBoxInLoginPopup = await $(loginForm.checkBox).isDisplayed();
+        expect(checkBoxInLoginPopup).to.equal(true);
+     });
+     it ('test verification the button socialsLogin Google in form "Login"', async function () {
+        const  inputWithAccauntGoogleInLoginPopup = await $(loginForm.inputWithAccauntGoogle).isDisplayed();
+        expect(inputWithAccauntGoogleInLoginPopup).to.equal(true);
+     });
+     it ('test verification the button socialsLogin Facebook in form "Login"', async function () {
+        const inputWithAccauntFacebookInLoginPopup = await $(loginForm.inputWithAccauntFacebook).isDisplayed();
+        expect(inputWithAccauntFacebookInLoginPopup).to.equal(true);
+     });
+     it ('test verification the button socialsLogin Apple in form "Login"', async function () {
+        const inputWithAccauntAppleInLoginPopup = await $(loginForm.inputWithAccauntApple).isDisplayed();
+        expect(inputWithAccauntAppleInLoginPopup).to.equal(true);
+     });
 
-        // await  estimate.numberfInstances.fillNumber();
-        // await  browser.switchToFrame($("null"));
-        // await  browser.switchToFrame($'([src*="calculator"][name="goog_983964356"])');
-        // // await  browser.switchToFrame("iframe#myFrame");
-        //
-        // await $("#input_91").setValue(4);
-        // await $("#select_value_label_83 div");
 
-//     });
-//
+
 });
